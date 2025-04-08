4b46a50da77d89e9ab9538e51480ee6e1356d7fe
denys-hw <denyspugachovhw@gmail.com>
2025-04-08 16:46:13 +0000

    add simple script

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,27 +1,7 @@
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
+ChromeOptions options = new ChromeOptions();
+options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
 
-import static org.junit.jupiter.api.Assertions.*;
-
-public class GoogleTest {
-
-    @Test
-    public void openGoogleAndCheckTitle() {
-        WebDriverManager.chromedriver().setup();
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-
-        String title = driver.getTitle();
-        System.out.println("Page title is: " + title);
-        assertTrue(title.contains("Google"));
-
-        driver.quit();
-    }
-}
+WebDriver driver = new ChromeDriver(options);
+driver.get("https://example.com");
+System.out.println("Title: " + driver.getTitle());
+driver.quit();
